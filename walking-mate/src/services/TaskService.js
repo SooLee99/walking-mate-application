04b700bdab51fd726f43eee4dd8d8ea3bef731a9
LeeSooo <dltn990930@naver.com>
2023-08-06 20:22:19 +0900

    feat: #7 - 일정, 운동 기록 서버 연결 구현 (2023-08-06 이수)
    일정 관리 및 운동 기록을 서버에서 가져올 수 있도록 코드를 구현하였습니다. 이를 바탕으로 추후 백엔드와 연동할 수 있습니다.

---
+++ walking-mate/src/services/TaskService.js
@@ -0,0 +1,215 @@
+/**
+ * @파일명 TaskService.js
+ * @작성자 이수
+ * @작성일 2023-07-27
+ */
+
+import { Alert } from 'react-native';
+import axios from 'axios';
+import { API_URL } from '../config/Config';
+
+export const TaskService = {
+  // (1) 일정 조회 처리 (2023-07-27 이수)
+  getTasks: async (id, date) => {
+    console.log('일정을 조회하였습니다.');
+    console.log(id.uid);
+    console.log(date);
+    try {
+      // 실제로는 서버에 요청을 보내는 코드가 필요합니다.
+      // const response = await axios.get(`${API_URL}/run/list/{date}`);
+
+      // 날짜별로 임시 일정 데이터를 설정(2023-07-28 이수)
+      const tasksData = {
+        status: 'OK',
+        message: '데이터베이스 조회 성공',
+        data: [
+          {
+            listId: 7,
+            userId: 'aaa',
+            date: '2023-08-08',
+            checked: false,
+            content: '체크리스트 테스트1',
+          },
+          {
+            listId: 8,
+            userId: 'aaa',
+            date: '2023-08-08',
+            checked: false,
+            content: '체크리스트 테스트2',
+          },
+          {
+            listId: 9,
+            userId: 'aaa',
+            date: '2023-08-08',
+            checked: false,
+            content: '체크리스트 테스트3',
+          },
+          {
+            listId: 9,
+            userId: 'aaa',
+            date: '2023-08-08',
+            checked: false,
+            content: '체크리스트 테스트4',
+          },
+          {
+            listId: 9,
+            userId: 'aaa',
+            date: '2023-08-08',
+            checked: false,
+            content: '체크리스트 테스트5',
+          },
+        ],
+      };
+      return tasksData;
+    } catch (error) {
+      console.error('Get tasks error:', error);
+      Alert.alert('Get Tasks Error', error.message);
+      throw error;
+    }
+  },
+
+  // (2) 일정 추가 처리 (2023-07-27 이수)
+  addTask: async (id, date, task) => {
+    console.log('해당 일정을 추가하였습니다.');
+    console.log(id.uid);
+    console.log(date);
+    console.log(task);
+    try {
+      // 실제로는 서버에 요청을 보내는 코드가 필요합니다.
+      // const response = await axios.post('${API_URL}/checkList/save', { id, date, task });
+      const response = {
+        status: 'OK',
+        message: '체크리스트 작성 성공',
+        data: {
+          listId: 7,
+          userId: 'aaa',
+          date: '20230808',
+          checked: false,
+          content: '체크리스트 테스트1',
+        },
+      }; // <- 임시로 성공 처리(2023-07-27 이수)
+      return response;
+    } catch (error) {
+      console.error('Add task error:', error);
+      Alert.alert('Add Task Error', error.message);
+      throw error;
+    }
+  },
+
+  // (3) 일정 삭제 처리 (2023-07-27 이수)
+  deleteTask: async (id, date, listId) => {
+    console.log('해당 일정을 삭제하였습니다.');
+    console.log(id.uid);
+    console.log(date);
+    console.log(listId);
+    try {
+      // 실제로는 서버에 요청을 보내는 코드가 필요합니다.
+      // const response = await axios.delete('${API_URL}/checkList/{listId}', { listId });
+      const response = {
+        status: 'OK',
+        message: '체크리스트 삭제 성공',
+        data: {
+          listId: 7,
+          userId: 'aaa',
+          date: '20230808',
+          checked: true,
+          content: 'update',
+        },
+      }; // <- 임시로 성공 처리(2023-07-27 이수)
+      return response;
+    } catch (error) {
+      console.error('Delete task error:', error);
+      Alert.alert('Delete Task Error', error.message);
+      throw error;
+    }
+  },
+
+  // (4) 일정 체크 상태 변동 (2023-08-12)
+  updateTaskCheckStatus: async (uid, selectedDate, listId) => {
+    console.log('해당 일정을 수정하였습니다.');
+    console.log(uid.uid);
+    console.log(selectedDate);
+    console.log(listId);
+    try {
+      // 실제로는 서버에 요청을 보내는 코드가 필요합니다.
+      // const response = await axios.delete('${API_URL}/checkList/checked/{listId}', { listId });
+      const response = {
+        status: 'OK',
+        message: '체크리스트 선택 및 해제',
+        data: {
+          listId: 7,
+          userId: 'aaa',
+          date: '20230808',
+          checked: true,
+          content: 'update',
+        },
+      }; // <- 임시로 성공 처리(2023-08-12 이수)
+      return response;
+    } catch (error) {
+      console.error('Update task check status error:', error);
+      Alert.alert('Update Task Check Status Error', error.message);
+      throw error;
+    }
+  }, // (5) 운동 기록 조회 처리 (2023-08-12 이수)
+  getExerciseRecords: async (userId) => {
+    console.log('운동 기록을 조회하였습니다.');
+    console.log(userId.uid);
+    try {
+      // 실제로는 서버에 요청을 보내는 코드가 필요합니다.
+      // const response = await axios.get(`${API_URL}/run/list`);
+
+      const exerciseRecordsData = {
+        status: 'OK',
+        message: '데이터베이스 조회 성공',
+        data: [
+          {
+            id: 1,
+            userId: 'aaa',
+            date: '2023-08-20',
+            step: 1000,
+            distance: 5.0,
+            time: 10,
+          },
+          {
+            id: 2,
+            userId: 'aaa',
+            date: '2023-08-21',
+            step: 555,
+            distance: 55.0,
+            time: 10,
+          },
+          {
+            id: 3,
+            userId: 'aaa',
+            date: '2023-08-23',
+            step: 555,
+            distance: 55.0,
+            time: 10,
+          },
+          {
+            id: 6,
+            userId: 'aaa',
+            date: '2023-08-24',
+            step: 200,
+            distance: 20.0,
+            time: 10,
+          },
+          {
+            id: 7,
+            userId: 'aaa',
+            date: '2023-08-28',
+            step: 2500,
+            distance: 25.0,
+            time: 10,
+          },
+        ],
+      };
+
+      return exerciseRecordsData;
+    } catch (error) {
+      console.error('Get exercise records error:', error);
+      Alert.alert('Get Exercise Records Error', error.message);
+      throw error;
+    }
+  },
+};
