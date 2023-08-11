172012b3b82eed62e10891f8e2b1b5c7601b649c
LeeSooo <dltn990930@naver.com>
2023-08-11 20:22:19 +0900

    featL #5 - 대결 서버 연결 구현 (2023-08-11 이수)
    대결과 관련하여 서버와 연동될 수 있는 부분을 구현하였습니다. 이를 통해 추후 백엔드와 연결할 수 있습니다.

---
+++ walking-mate/src/services/MatchService.js
@@ -0,0 +1,202 @@
+/**
+ * @파일명 MatchService.js
+ * @작성자 이수
+ * @작성일 2023-08-03
+ */
+
+import { Alert } from 'react-native';
+import axios from 'axios';
+import { API_URL } from '../config/Config';
+
+export const MatchService = {
+  // (1) 모집 중인 팀 리스트의 정보를 가져오는 함수
+  getRecruitingMatchList: async () => {
+    try {
+      // 실제로는 서버에 요청을 보내는 코드가 필요합니다.
+      // const response = await axios.get('/battle/list');
+      const response = {
+        status: 'OK',
+        message: '데이터베이스 조회 성공',
+        data: [
+          {
+            id: 15,
+            startDate: null,
+            totalStep: 0,
+            battleCheck: '대결 팀 모집 중',
+            createdDate: '20230801',
+            battleRivals: [
+              {
+                teamId: 8,
+                teamLeader: 'aaa',
+                teamTier: '실버',
+                intro: '안녕하세용',
+                teamName: 'aaa의 팀',
+                peopleNum: 4,
+                step: 0,
+              },
+            ],
+          },
+          {
+            id: 16,
+            startDate: null,
+            totalStep: 0,
+            battleCheck: '대결 팀 모집 중',
+            createdDate: '20230802',
+            battleRivals: [
+              {
+                teamId: 9,
+                teamLeader: 'bbb',
+                teamTier: '브론즈',
+                teamName: 'bbb의 팀',
+                intro: '안녕하세용',
+                peopleNum: 4,
+                step: 0,
+              },
+            ],
+          },
+          {
+            id: 17,
+            startDate: null,
+            totalStep: 0,
+            battleCheck: '대결 팀 모집 중',
+            createdDate: '20230802',
+            battleRivals: [
+              {
+                teamId: 10,
+                teamLeader: 'ccc',
+                teamTier: '골드',
+                teamName: 'ccc의 팀',
+                intro: '안녕하세용',
+                peopleNum: 4,
+                step: 0,
+              },
+            ],
+          },
+          {
+            id: 18,
+            startDate: '2023-08-01',
+            totalStep: 400,
+            battleCheck: '대결 진행 중',
+            battleRivals: [
+              {
+                teamId: 11,
+                teamName: 'ddd의 팀',
+                teamTier: '실버',
+                intro: '안녕하세용',
+                peopleNum: 4,
+                step: 100,
+              },
+              {
+                teamId: 12,
+                teamName: 'eee의 팀',
+                teamTier: '실버',
+                intro: '안녕하세용',
+                peopleNum: 4,
+                step: 300,
+              },
+            ],
+          },
+        ],
+      };
+      return response.data;
+    } catch (error) {
+      console.error('Error in getting recruiting teams:', error);
+      Alert.alert('Error', error.message);
+      throw error;
+    }
+  },
+
+  //(2) 사용자의 팀이 현재 대결 중인지 확인하는 함수 (2023-07-29 이수)
+  isUserInMatch: async (id) => {
+    try {
+      // 서버에 요청하는 부분은 주석 처리하였습니다.
+      // const response = await axios.get('/api/user/teamStatus');
+      // const data = await response.data;
+      // return data;
+
+      const response = {
+        status: 'OK',
+        message: '데이터베이스 조회 성공',
+        data: {
+          id: 18,
+          startDate: '2023-08-01',
+          totalStep: 400,
+          battleCheck: '대결 진행 중',
+          battleRivals: [
+            {
+              teamId: 11,
+              teamName: 'ddd의 팀',
+              teamTier: '실버',
+              intro: '안녕하세용',
+              peopleNum: 4,
+              step: 100,
+            },
+            {
+              teamId: 12,
+              teamName: 'eee의 팀',
+              teamTier: '실버',
+              intro: '안녕하세용',
+              peopleNum: 4,
+              step: 300,
+            },
+          ],
+        },
+      };
+
+      const response1 = {
+        status: 'OK',
+        message: '데이터베이스 조회 성공',
+        data: {
+          id: 15,
+          startDate: null,
+          totalStep: 0,
+          battleCheck: '팀 생성 완료',
+          battleRivals: [
+            {
+              teamId: 8,
+              teamName: 'aaa의 팀',
+              peopleNum: 4,
+              step: 0,
+            },
+          ],
+        },
+      };
+      return response1;
+    } catch (error) {
+      console.error('Error in checking if user is a team leader:', error);
+      Alert.alert('Error', error.message);
+      throw error;
+    }
+  },
+
+  // (3) 대결 생성 처리 (2023-07-31 이수)
+  createMatch: async (matchData) => {
+    try {
+      /*const response = await fetch('/battle/new', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(matchData)
+      });*/
+
+      const response = {
+        status: 'OK',
+        message: '대결 생성 성공',
+        data: {
+          id: 16,
+          startDate: null,
+          totalStep: 0,
+          battleCheck: '팀 모집 중',
+          battleRivals: null,
+        },
+      };
+
+      return response;
+    } catch (error) {
+      console.error('Error in createMatch:', error);
+      throw error;
+    }
+  },
+};
