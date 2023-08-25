4f086828f36011c6ae08d16ac52233bb05d99655
LeeSooo <dltn990930@naver.com>
2023-08-25 19:37:43 +0900

    feat: #2 - 로그인 서버 연결 구현 (2023-07-27 이수)
    로그인 화면과 서버를 연결하는 코드를 구현하여, 추후 서버와 연결을 가능하게 합니다.

--- walking-mate/src/App.js
+++ walking-mate/src/App.js
@@ -3,50 +3,53 @@
  * @작성자 이수
  * @작성일 2023-07-23
  */
-
-import React, { useEffect, useState } from 'react';
-import { StatusBar, BackHandler, Alert } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar, Platform, PermissionsAndroid } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './themes/theme';
 import Navigation from './navigations';
 import { UserProvider, ProgressProvider } from './contexts';
 
-const App = () => {
-  const [exitApp, setExitApp] = useState(false);
-
-  useEffect(() => {
-    const backAction = () => {
-      if (exitApp == false) {
-        setExitApp(true);
-        setTimeout(() => {
-          setExitApp(false);
-        }, 2000);
-        return true;
+const requestLocationPermission = async () => {
+  if (Platform.OS === 'android') {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: '위치 권한 요청',
+          message: '이 앱은 귀하의 위치에 접근할 권한이 필요합니다.',
+          buttonNeutral: '나중에 묻기',
+          buttonNegative: '취소',
+          buttonPositive: '확인',
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('위치 접근이 허용되었습니다.');
       } else {
-        BackHandler.exitApp();
+        console.log('위치 권한이 거부되었습니다.');
       }
-    };
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+};
 
-    const backHandler = BackHandler.addEventListener(
-      'hardwareBackPress',
-      backAction
-    );
-
-    return () => backHandler.remove();
-  }, [exitApp]);
-
+const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <UserProvider>
-        <UserProvider>
-          <StatusBar
-            backgroundColor={theme.background}
-            barStyle="dark-content"
-          />
-          <Navigation />
-        </UserProvider>
-      </UserProvider>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <ProgressProvider>
+          <UserProvider>
+            <StatusBar
+              backgroundColor={theme.background}
+              barStyle="dark-content"
+            />
+            <Navigation />
+          </UserProvider>
+        </ProgressProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 };
 
--- walking-mate/src/hooks/useLogin.js
+++ walking-mate/src/hooks/useLogin.js
@@ -4,17 +4,25 @@
  * @작성일 2023-07-23
  */
 
+import { useContext } from 'react';
+import { UserContext } from '../contexts/User';
 import { useState } from 'react';
-import { AuthService } from '../services/AuthService';
+import { UserAuthService } from '../services/UserAuthService';
 
 export const useLogin = () => {
+  const { setUser } = useContext(UserContext);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const login = async (id, pw) => {
     setLoading(true);
+    console.log('LOG  로그인 시도:', id);
     try {
-      const response = await AuthService.login(id, pw);
+      const response = await UserAuthService.login(id, pw);
+      console.log('LOG  로그인 응답:', response);
+      if (response.success) {
+        setUser({ uid: response.uid.uid, email: id });
+      }
       setLoading(false);
       return response;
     } catch (error) {
